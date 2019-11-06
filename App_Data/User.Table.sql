435962061d20f571d8e65eac1b13af03290c8277
tevon <tevondavisjc@gmail.com>
2019-11-06 03:14:53 -0100

    Initial commit

---
+++ App_Data/User.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [full_name] VARCHAR(50) NOT NULL, 
+    [email] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL, 
+    [comment] VARCHAR(50) NULL, 
+    [active] VARCHAR(50) NULL
+)
